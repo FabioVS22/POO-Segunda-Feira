948fb3256f4a679a1c58dca327caaa9f3dbb42e7
FabioVS22 <fabiovsantos22@uni9.edu.br>
2025-09-02 00:32:34 +0000

    feat: add projeto

--- Program.cs
+++ Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Televisao tv = new Televisao(60f);
+
+
+Console.WriteLine($"A tv tem o tamanho {tv.Tamanho}");
+
+tv.Volume = -35;
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.Volume = 100;
+Console.WriteLine($"Volume {tv.Volume}");
+
+
+
